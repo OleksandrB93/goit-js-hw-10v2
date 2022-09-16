55dff2f1418ebb0e8788f422280ed0fa0dd77664
Oleksandr Barabash <barabashsan4ik@gmail.com>
2022-09-16 15:51:46 +0300

    comment all js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,14 @@
-const URL = 'https://restcountries.com';
+// const URL = 'https://restcountries.com';
 
-const fetchCountries = function (name) {
-  return fetch(
-    `${URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (response.status === 404) {
-      return Promise.reject(new Error());
-    }
-    return response.json();
-  });
-};
+// const fetchCountries = function (name) {
+//   return fetch(
+//     `${URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+//   ).then(response => {
+//     if (response.status === 404) {
+//       return Promise.reject(new Error());
+//     }
+//     return response.json();
+//   });
+// };
 
-export { fetchCountries };
+// export { fetchCountries };
