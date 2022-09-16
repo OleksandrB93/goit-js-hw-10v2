c8b352aad9c7d928162120102a8e712d8809a2db
Oleksandr Barabash <barabashsan4ik@gmail.com>
2022-09-16 15:53:57 +0300

    comment all without css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import './css/styles.css';
+import './css/styles.css';
 // import { fetchCountries } from './fetchCountries';
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import debounce from 'lodash.debounce';
